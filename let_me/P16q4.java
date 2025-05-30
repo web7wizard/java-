e66755c05018619935f00b1a99e269fb3cc74b8f
Jiya <164993852+web7wizard@users.noreply.github.com>
2025-05-30 22:16:39 +0530

    Add files via upload

---
+++ let_me/P16q4.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+public class P16q4 extends JFrame
+{
+  JButton e=new JButton("east");
+  JButton w=new JButton("west");
+  JButton s=new JButton("south");
+  JButton n=new JButton("north");
+  JButton c=new JButton("center");
+  jbut()
+  {
+   setLayout(new BorderLayout(10,10);
+   add(e.BorderLayout.EAST);
+   add(w.BorderLayout.WEST);
+   add(s.BorderLayout.SOUTH);
+   add(n.BorderLayout.NORTH);
+   add(c.BorderLayout.CENTER);'
+ }
+  public static void main(String a[])
+  {
+   P16q4 p=new P16q4();
+   p.setSize(300,300);
+   p.setVisible(true);
+ }
+}
+   
+
+
+  
+  
